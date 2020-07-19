2f37f371960c54d151de302690615398bb78d38d
Дмитрий <dapavlov16@yandex.ru>
2020-07-19 21:41:53 +0300

    Initial Commit

---
+++ HomeworkVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkVIPER
+//
+//  Created by Дмитрий Павлов on 19.07.2020.
+//  Copyright © 2020 Дмитрий Павлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
